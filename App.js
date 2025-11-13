3777cfedc551352769625f3830a9f79f953fcaf9
최민수 <mandoo4137@gmail.com>
2025-11-13 21:30:04 +0900

    RN 스타일링

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.dummyText}>another peace of text!</Text>
+      </View>
+      <Text
+        style={{ margin: 16, borderWidth: 2, borderColor: "red", padding: 16 }}
+      >
+        Hello world
+      </Text>
+      <Button title="Tap me!" />
     </View>
   );
 }
@@ -16,5 +22,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  dummyText: {
+    margin: 16,
+    padding: 16,
+    borderWidth: 2,
+    borderColor: "blue",
   },
 });
